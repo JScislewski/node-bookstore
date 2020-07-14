b26a8dc1747b5fdf7a8305fb5d2f76c07c8a650a
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-07-14 16:28:11 +0200

    Add 404 page

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
